93ba3a0f12b63af2c225e845102a34a09f7cea66
D Seidel <67442972+shykitten27@users.noreply.github.com>
2021-06-21 22:20:47 -0500

    textbook exercises

--- angular_practice/my-project-name/src/app/app.component.ts
+++ angular_practice/my-project-name/src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'donna-project';
+  title = 'donna-project-name';
+  name: string = 'Barbara Liskov';
+  itemList: string[] = ['item1', 'item2', 'item3', 'item4'];
+  
+  rectangle = {
+    length: 5,
+    width: 6,
+    area: function() {
+       return this.length * this.width;
+    }
+  }
 }
