404a9d7b25c4b0fb8bfa370611dcb94ba2981bfd
D Seidel <67442972+shykitten27@users.noreply.github.com>
2021-06-26 02:33:33 -0500

    dev

--- lesson2/examples/ngfor-practice/src/app/chores/chores.component.ts
+++ lesson2/examples/ngfor-practice/src/app/chores/chores.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChoresComponent implements OnInit {
    chores = [
-   {title: "Yesterday's Chores", tasks: ['Empty dishwasher', 'Start LaunchCode prep work', 'Buy groceries']},
+   {title: "Yesterday's Chores", tasks: ['Empty dishwasher', 'Start LaunchCode prep work']},
    {title: "Today's Chores", tasks: ['Load dishwasher', 'Finish LaunchCode prep work', 'Buy the groceries you forgot']},
    {title: "Tomorrow's Chores", tasks: ['Empty dishwasher AGAIN', 'Play with LaunchCode practice code', 'Groceries AGAIN']},
 ]
