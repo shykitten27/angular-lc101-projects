48740bb52f88576011be958f6cf941b2695560e6
D Seidel <67442972+shykitten27@users.noreply.github.com>
2021-07-01 21:34:35 -0500

    add warning function add sort function

--- orbit-report/src/app/satellite.ts
+++ orbit-report/src/app/satellite.ts
@@ -15,10 +15,10 @@ export class Satellite {
     }
 
     shouldShowWarning(): boolean {
-        if(this.type.toLowerCase() === 'space debris'){
+        if(this.type.toLowerCase() === 'space debris') {
             return true;
         }else{
-          return false;  
+            return false;  
         }
     }
 
