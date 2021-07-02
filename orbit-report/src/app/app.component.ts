6079da842fc01bb96512aa89d3af4f796fd986b5
D Seidel <67442972+shykitten27@users.noreply.github.com>
2021-07-01 22:14:51 -0500

    add search feature

--- orbit-report/src/app/app.component.ts
+++ orbit-report/src/app/app.component.ts
@@ -9,8 +9,10 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'orbit-report';
   sourceList: Satellite[];
+  displayList: Satellite[]; 
 constructor() {
    this.sourceList = [];
+   this.displayList = []
    let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
 
    window.fetch(satellitesUrl).then(function(response) {
@@ -29,10 +31,23 @@ constructor() {
                                       fetchedSatellites[i].operational);
           this.sourceList.push(satellite); //push new object
         }
-
+      // make a copy of the sourceList to be shown to the user
+      this.displayList = this.sourceList.slice(0);
       }.bind(this));
    }.bind(this));
 
 }
+search(searchTerm: string): void {
+  let matchingSatellites: Satellite[] = [];
+  searchTerm = searchTerm.toLowerCase();
+  for(let i=0; i < this.sourceList.length; i++) {
+     let name = this.sourceList[i].name.toLowerCase();
+     if (name.indexOf(searchTerm) >= 0) {
+        matchingSatellites.push(this.sourceList[i]);
+     }
+  }
+  // assign this.displayList to be the array of matching satellites
+  // this will cause Angular to re-make the table, but now only containing matches
+  this.displayList = matchingSatellites;
 }
-
+}
